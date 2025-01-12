839f0ff4e56e41555a4aa2efa2364a3855f80c30
sponky <jay.was.herex69@gmail.com>
2025-01-12 03:00:46 -0800

    Update main.css/main.js
    Added the sliding transition to the aside content :smiley:

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-document.getElementById('toggle-aside').addEventListener('click', function() {
-    var aside = document.getElementById('aside-section');
-    if (aside.style.display === 'none' || aside.style.display === '') {
-        aside.style.display = 'block';
-    } else {
-        aside.style.display = 'none';
-    }
+const aside = document.getElementById('aside-section');
+const toggleButton = document.getElementById('toggle-aside');
+
+toggleButton.addEventListener('click', () => {
+    aside.classList.toggle('open');
 });
